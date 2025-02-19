dec39794aed85ff8aecc9163481cfbcc5c3c5d23
nonbots <nhantdbui@gmail.com>
2025-02-19 12:54:30 -0500

    fixed indentation irc commandsHandler

--- src/twitch/irc/commandsHandler.ts
+++ src/twitch/irc/commandsHandler.ts
@@ -17,41 +17,41 @@ export async function commandsHandler( parsedMessage: ParsedMessage, client: Web
                     if (!isValidURL(url)) {
                         client.send(`PRIVMSG ${channel} : @${chatter}, invalid Youtube video url.`);
                     }
-                const video = await getVideo(url, auth.YT_API_KEY);
-                chatQueue.add(video);
-                const firstVideo = chatQueue.getFirst();
-                if (firstVideo) client.send(`PRIVMSG ${channel} : @${chatter}, ${firstVideo.title} added to chat queue.`);
-            } catch (error) {
-            if (error instanceof TypeError) {
-                if (error.message === "Invalid URL") {
-                client.send(`PRIVMSG ${channel} : @${chatter}, invalid url.`);
+                    const video = await getVideo(url, auth.YT_API_KEY);
+                    chatQueue.add(video);
+                    const firstVideo = chatQueue.getFirst();
+                    if (firstVideo) client.send(`PRIVMSG ${channel} : @${chatter}, "${firstVideo.title}" added to chat queue.`);
+                } catch (error) {
+                    if (error instanceof TypeError) {
+                        if (error.message === "Invalid URL") {
+                        client.send(`PRIVMSG ${channel} : @${chatter}, invalid url.`);
+                        }
+                    } else if (error instanceof VideoError) {
+                        switch(error.message) {
+                        case "liveStreamRestriction":
+                            client.send(`PRIVMSG ${channel} : @${chatter}, live streams are restricted.`);
+                            break;
+                        case "ageRestriction":
+                            client.send(`PRIVMSG ${channel} : @${chatter}, video is age restricted.`);
+                            break;
+                        case "regionRestriction":
+                            client.send(`PRIVMSG ${channel} : @${chatter}, video is age restricted in the US.`);
+                            break;
+                        case "notEmbeddable":
+                            client.send(`PRIVMSG ${channel} : @${chatter}, video can't be played on embedded player.`);
+                            break;
+                        case "durationRestriction":
+                            client.send(`PRIVMSG ${channel} : @${chatter}, video duration can't be over 10 minutes.`);
+                            break;
+                        default:
+                            client.send(`PRIVMSG ${channel} : @${chatter}, sorry, something went wrong with the request.`);
+                            console.error(error);
+                        }
+                    }
                 }
-            } else if (error instanceof VideoError) {
-                switch(error.message) {
-                case "liveStreamRestriction":
-                    client.send(`PRIVMSG ${channel} : @${chatter}, live streams are restricted.`);
-                    break;
-                case "ageRestriction":
-                    client.send(`PRIVMSG ${channel} : @${chatter}, video is age restricted.`);
-                    break;
-                case "regionRestriction":
-                    client.send(`PRIVMSG ${channel} : @${chatter}, video is age restricted in the US.`);
-                    break;
-                case "notEmbeddable":
-                    client.send(`PRIVMSG ${channel} : @${chatter}, video can't be played on embedded player.`);
-                    break;
-                case "durationRestriction":
-                    client.send(`PRIVMSG ${channel} : @${chatter}, video duration can't be over 10 minutes.`);
-                    break;
-                default:
-                    client.send(`PRIVMSG ${channel} : @${chatter}, sorry, something went wrong with the request.`);
-                    console.error(error);
-                }
+            } else {
+                client.send(`PRIVMSG ${channel} : @${chatter}, no url provided.`);
             }
-            }
-        } else {
-            client.send(`PRIVMSG ${channel} : @${chatter}, no url provided.`);
-        }
         }
     }
 }
